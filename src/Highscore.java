de13af2fc1d4103f35f9c8f8fe5955f87f4f392e
Shivanireddy25 <shivanireddy.singireddy@sjsu.edu>
2019-04-19 17:55:49 -0700

    Initial code commit for game setup

---
+++ src/Highscore.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+//import java.awt.Color;
+import java.awt.Font;
+public class Highscore extends Actor
+{
+    private Board board=new Board();
+    private String text ="Score: 0";
+    private GreenfootImage image;
+
+    public Highscore(int pScore, boolean gameOver) //Zeigt den Highscore in verschiedenen Größen an, je nach Spielstatus (Game Over oder nicht)
+    {
+        text = "Highscore: "+pScore;
+        if (gameOver)
+        {
+            printText(text,50);
+        }
+        else
+        {
+            printText(text,30);
+        }
+    }
+
+    public void printText(String pValue,int pSize) //Setzt den Text asl Bild
+    {
+        setImage(new GreenfootImage(pValue, pSize, Color.WHITE,new Color(0,0,0,0)));
+    }
+}
