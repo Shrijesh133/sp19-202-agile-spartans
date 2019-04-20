632a7df72b76f054b2188c116744c5903f1a87e9
vaibhav gupta <gupta.vaibhav1911@gmail.com>
2019-04-20 06:28:25 +0530

    Initial game files commit (after understanding)
    Initial game files commit (after understanding)

---
+++ src/Logo.java
@@ -0,0 +1,11 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Logo extends Actor
+{
+    GreenfootImage image = getImage();  
+    public Logo()
+    {
+        image.scale(200, 200);
+        setImage(image);
+    }
+}
