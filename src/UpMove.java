7f0c5241c0db9babd968d71db78486dcc64a7874
deepikay912 <deepika640yannamani@gmail.com>
2019-04-22 15:39:26 -0700

    algorithm for up move
    algorithm for up move

---
+++ src/UpMove.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class UpMove extends Move {
+
+    @Override
+    int moveOperation(Field[][] field) {
+
+        int score  = 0;
+
+        for (y=1; y< field.length; y++) //Nested for loop to manipulate the respective fields of the array
+        {
+            for (x=0; x< field.length; x++)
+            {
+                for (int i=1; i< field.length; i++) // Another for loop, in case a field can be moved by several units
+                {
+                    if (field[x][i].getValue()!=0&&field[x][i-1].getMoved()==false&&field[x][i].getMoved()==false&&(field[x][i-1].getValue()==field[x][i].getValue()||field[x][i-1].getValue()==0))
+                    {
+                        if(field[x][i-1].getValue()!=0)
+                        {
+                            score = score + field[x][i].getValue()*2//Add score
+                            //field[x][i].setMoved(true);
+                            field[x][i-1].setMoved(true); //moved is set to true so that no fields are added twice
+                        }
+                        field[x][i-1].setValue(field[x][i].getValue()+field[x][i-1].getValue()); //Add values
+                        field[x][i].setValue(0); //Sets previous value to 0
+                        this.anyFieldsMoved=true; //Fields were moved
+
+                    }
+                }
+            }
+        }
+    return  score;
+    }
+
+}
