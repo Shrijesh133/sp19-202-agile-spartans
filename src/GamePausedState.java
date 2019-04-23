18dbdca7e43cf015855ddf1714897dedd63d3cdd
Shivanireddy25 <shivanireddy.singireddy@sjsu.edu>
2019-04-23 09:25:58 -0700

    Implemented state pattern to maintain game state

---
+++ src/GamePausedState.java
@@ -0,0 +1,32 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GameRunningState here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GamePausedState extends GameState implements IGameState
+{
+    /**
+     * Act - do whatever the GameRunningState wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    } 
+    
+    
+    public void restart() {
+    }
+    
+    public void play(){
+    }
+    
+    public void pause(){
+    }
+    
+    public void resume() {
+    }
+}
---
+++ src/GameRunningState.java
@@ -0,0 +1,33 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GameRunningState here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GameRunningState extends GameState 
+{
+    /**
+     * Act - do whatever the GameRunningState wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    } 
+    
+    
+    public void restart() {
+    }
+    
+    public void play(){
+        System.out.println("Game Running");
+    }
+    
+    public void pause(){
+    }
+    
+    public void resume() {
+    }
+}
