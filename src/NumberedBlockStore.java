3b542cbf0743f4b2870e8e8196e9e760afc5f75e
vaibhav gupta <gupta.vaibhav1911@gmail.com>
2019-04-26 10:53:17 +0530

    Prototype pattern for number block
    Prototype pattern for number block

---
+++ src/NumberedBlockStore.java
@@ -0,0 +1,33 @@
+/**
+ * Write a description of class NumberedBlockStore here. 
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.HashMap;
+import java.util.Map;
+
+public class NumberedBlockStore  
+{
+ private static Map<String, NumberedBlock> NumberedBlockMap = new HashMap<String, NumberedBlock>();  
+       
+    static 
+    { 
+        NumberedBlockMap.put("NumberedBlock2", new TwoNumberedBlock(2)); 
+        NumberedBlockMap.put("NumberedBlock4", new TwoNumberedBlock(4)); 
+        NumberedBlockMap.put("NumberedBlock8", new TwoNumberedBlock(8)); 
+        NumberedBlockMap.put("NumberedBlock16", new TwoNumberedBlock(16)); 
+        NumberedBlockMap.put("NumberedBlock32", new TwoNumberedBlock(32)); 
+        NumberedBlockMap.put("NumberedBlock64", new TwoNumberedBlock(64)); 
+        NumberedBlockMap.put("NumberedBlock128", new TwoNumberedBlock(128)); 
+        NumberedBlockMap.put("NumberedBlock256", new TwoNumberedBlock(256)); 
+        NumberedBlockMap.put("NumberedBlock512", new TwoNumberedBlock(512)); 
+        NumberedBlockMap.put("NumberedBlock1024", new TwoNumberedBlock(1024)); 
+        NumberedBlockMap.put("NumberedBlock2048", new TwoNumberedBlock(2048)); 
+    } 
+       
+    public static NumberedBlock getNumberedBlock(String colorName) 
+    { 
+        return (NumberedBlock) NumberedBlockMap.get(colorName).clone(); 
+    } 
+}
