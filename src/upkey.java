fae1b942061fd19dfc5ff1afe146d2dc5397b57c
DhruvilBParikh <47414810+DhruvilBParikh@users.noreply.github.com>
2019-04-27 02:14:46 -0700

    Solving Upkey error

--- src/upkey.java
+++ src/upkey.java
@@ -22,11 +22,10 @@ public class upkey extends Actor
             key = true;//Greenfoot.setWorld(new Game());
             board = (Board)getWorld().getObjects(Board.class).get(0);
             
-<<<<<<< HEAD
+
             board.notifyAll();
-=======
+
            // board.notifyact();
->>>>>>> master
         }
     }     
 }
