1e2426c9462644773eb2bfd7ff40e413307df2b3
vaibhav gupta <gupta.vaibhav1911@gmail.com>
2019-05-03 08:23:10 +0530

    Fixes for errors in game paused

--- src/GamePausedState.java
+++ src/GamePausedState.java
@@ -7,7 +7,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @version (a version number or a date)
  */
 public class GamePausedState extends GameState
-   
+  {
     public GamePausedState(Board b){
         super(b);
     }
